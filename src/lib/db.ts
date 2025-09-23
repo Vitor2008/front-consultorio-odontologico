38639f6c37651887dbb1e00c2687239d6bc06103
João Pedro <11joao44@gmail.com>
2025-09-23 17:45:15 -0400

    Feat: retirando credencial do código

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,7 @@
-// Ficheiro: src/lib/db.ts
 import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectionString =
-  "postgresql://neondb_owner:npg_gXO8mVk6MdJP@ep-empty-wind-acljaddl-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-
-export const pool = new Pool({
-  connectionString,
-});
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 console.log("Pool de conexões com o banco de dados criado com sucesso!");
