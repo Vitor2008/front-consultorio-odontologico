cc85aa1672023ade302bc88fcf268f4860052b4e
João Pedro <11joao44@gmail.com>
2025-09-25 23:10:09 -0400

    Update: reaforação no Lazy e adicionado cofig vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Layout from "./Pages/Layout/Layout";
-import Consultas from "./Pages/Consultas/Consultas";
-import Pacientes from "./Pages/Pacientes/Pacientes";
-import Medicos from "./Pages/Dentistas/Dentistas";
-import Financeiro from "./Pages/Financeiro/Financeiro";
-import Configuracoes from "./Pages/Configuracoes/Configuracoes";
 import Loader from "./Components/Loader/Loader";
 
 const Login = lazy(() => import("./Pages/Login/Login"));
+const Consultas = lazy(() => import("./Pages/Consultas/Consultas"));
+const Pacientes = lazy(() => import("./Pages/Pacientes/Pacientes"));
+const Medicos = lazy(() => import("./Pages/Dentistas/Dentistas"));
+const Financeiro = lazy(() => import("./Pages/Financeiro/Financeiro"));
+const Configuracoes = lazy(() => import("./Pages/Configuracoes/Configuracoes"));
+
+const AppLayout = () => (
+  <Layout>
+    <Outlet />
+  </Layout>
+);
 
 function App() {
   return (
-    <Suspense fallback={<div><Loader/></div>}>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/consultas"
-          element={
-            <Layout>
-              <Consultas />
-            </Layout>
-          }
-        />
-        <Route
-          path="/pacientes"
-          element={
-            <Layout>
-              <Pacientes />
-            </Layout>
-          }
-        />
-        <Route
-          path="/medicos"
-          element={
-            <Layout>
-              <Medicos />
-            </Layout>
-          }
-        />
-        <Route
-          path="/financeiro"
-          element={
-            <Layout>
-              <Financeiro />
-            </Layout>
-          }
-        />
-        <Route
-          path="/configuracoes"
-          element={
-            <Layout>
-              <Configuracoes />
-            </Layout>
-          }
-        />
+        <Route element={<AppLayout />}>
+          <Route path="/consultas" element={<Consultas />} />
+          <Route path="/pacientes" element={<Pacientes />} />
+          <Route path="/medicos" element={<Medicos />} />
+          <Route path="/financeiro" element={<Financeiro />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+        </Route>
       </Routes>
     </Suspense>
   );
